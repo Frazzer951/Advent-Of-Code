49c409d48539ef6e63510241b64f47f353db2a5f
Luke Eltiste <luke343279@gmail.com>
2021-10-22 13:35:33 -0700

    2018 day 18

--- 2015/18/code.py
+++ 2015/18/code.py
@@ -4,29 +4,117 @@
 
 with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
     input = input_file.read().split("\n")
+input = [list(line) for line in input]
 
 
-def part1(input):
-    pass
+def countNeighbors(board, x, y):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if x + i < 0 or x + i >= len(board) or y + j < 0 or y + j >= len(board[0]):
+                continue
+            if i == 0 and j == 0:
+                continue
+            if board[x + i][y + j] == "#":
+                count += 1
+    return count
 
 
-t1 = part1([""])
-print("Part One Test 1: " + str(t1))
-t2 = part1([""])
-print("Part One Test 2: " + str(t2))
+def nextStep(board):
+    newBoard = [["." for i in range(len(board[0]))] for j in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            neighbors = countNeighbors(board, i, j)
+            if board[i][j] == "#" and (neighbors == 2 or neighbors == 3):
+                newBoard[i][j] = "#"
+            if board[i][j] == "." and neighbors == 3:
+                newBoard[i][j] = "#"
+    return newBoard
 
-p1 = part1(input)
-print("Part One : " + str(p1))
+
+def iterateBoard(board, iterations):
+    for i in range(iterations):
+        board = nextStep(board)
+    return board
 
 
-def part2(input):
-    pass
+t1 = iterateBoard(
+    [
+        [".", "#", ".", "#", ".", "#"],
+        [".", ".", ".", "#", "#", "."],
+        ["#", ".", ".", ".", ".", "#"],
+        [".", ".", "#", ".", ".", "."],
+        ["#", ".", "#", ".", ".", "#"],
+        ["#", "#", "#", "#", ".", "."],
+    ],
+    4,
+)
+count = sum([lst.count("#") for lst in t1])
+assert t1 == [
+    [".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+    [".", ".", "#", "#", ".", "."],
+    [".", ".", "#", "#", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+]
+assert count == 4
+print("Part One Test 1: " + str(count))
+
+p1 = iterateBoard(input, 100)
+count = sum([lst.count("#") for lst in p1])
+print("Part One : " + str(count))
 
 
-t3 = part2([""])
-print("Part Two Test 1: " + str(t3))
-t4 = part2([""])
-print("Part Two Test 2: " + str(t4))
+def nextStep2(board):
+    newBoard = [["." for i in range(len(board[0]))] for j in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            neighbors = countNeighbors(board, i, j)
+            if board[i][j] == "#" and (neighbors == 2 or neighbors == 3):
+                newBoard[i][j] = "#"
+            if board[i][j] == "." and neighbors == 3:
+                newBoard[i][j] = "#"
+    newBoard[0][0] = "#"
+    newBoard[0][-1] = "#"
+    newBoard[-1][0] = "#"
+    newBoard[-1][-1] = "#"
+    return newBoard
 
-p2 = part2(input)
-print("Part Two : " + str(p2))
+
+def iterateBoard2(board, iterations):
+    board[0][0] = "#"
+    board[0][-1] = "#"
+    board[-1][0] = "#"
+    board[-1][-1] = "#"
+    for i in range(iterations):
+        board = nextStep2(board)
+    return board
+
+
+t3 = iterateBoard2(
+    [
+        ["#", "#", ".", "#", ".", "#"],
+        [".", ".", ".", "#", "#", "."],
+        ["#", ".", ".", ".", ".", "#"],
+        [".", ".", "#", ".", ".", "."],
+        ["#", ".", "#", ".", ".", "#"],
+        ["#", "#", "#", "#", ".", "#"],
+    ],
+    5,
+)
+count = sum([lst.count("#") for lst in t3])
+assert t3 == [
+    ["#", "#", ".", "#", "#", "#"],
+    [".", "#", "#", ".", ".", "#"],
+    [".", "#", "#", ".", ".", "."],
+    [".", "#", "#", ".", ".", "."],
+    ["#", ".", "#", ".", ".", "."],
+    ["#", "#", ".", ".", ".", "#"],
+]
+assert count == 17
+print("Part Two Test 1: " + str(count))
+
+p2 = iterateBoard2(input, 100)
+count = sum([lst.count("#") for lst in p2])
+print("Part Two : " + str(count))
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Advent of code problems solved
 * 2015
-    * Days 1-17
+    * Days 1-18
 * 2016
     * /
 * 2017
