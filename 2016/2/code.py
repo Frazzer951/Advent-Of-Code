28052bc19d189f66179a3039ea9db9370284507c
Luke Eltiste <luke343279@gmail.com>
2021-11-01 08:42:29 -0700

    2016 Day 2

--- 2016/2/code.py
+++ 2016/2/code.py
@@ -2,31 +2,51 @@
 # Author = Frazzer951
 # Date = November 2021
 
+import math
+
 with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
     input = input_file.read().split("\n")
 
-
-def part1(input):
-    pass
+print(input)
 
 
-t1 = part1([""])
+def decipherCode(input, keypad=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], x=1, y=1):
+    code = ""
+    for line in input:
+        for dir in line:
+            if dir == "U":
+                if y > 0 and keypad[y - 1][x] != None:
+                    y -= 1
+            elif dir == "D":
+                if y < len(keypad) - 1 and keypad[y + 1][x] != None:
+                    y += 1
+            elif dir == "L":
+                if x > 0 and keypad[y][x - 1] != None:
+                    x -= 1
+            elif dir == "R":
+                if x < len(keypad[0]) - 1 and keypad[y][x + 1] != None:
+                    x += 1
+        code += str(keypad[y][x])
+    return code
+
+
+t1 = decipherCode(["ULL", "RRDDD", "LURDL", "UUUUD"])
 print("Part One Test 1: " + str(t1))
-t2 = part1([""])
-print("Part One Test 2: " + str(t2))
+assert t1 == "1985"
 
-p1 = part1(input)
+p1 = decipherCode(input)
 print("Part One : " + str(p1))
 
-
-def part2(input):
-    pass
-
-
-t3 = part2([""])
+keypad = [
+    [None, None, 1, None, None],
+    [None, 2, 3, 4, None],
+    [5, 6, 7, 8, 9],
+    [None, "A", "B", "C", None],
+    [None, None, "D", None, None],
+]
+t3 = decipherCode(["ULL", "RRDDD", "LURDL", "UUUUD"], keypad, 0, 2)
 print("Part Two Test 1: " + str(t3))
-t4 = part2([""])
-print("Part Two Test 2: " + str(t4))
+assert t3 == "5DB3"
 
-p2 = part2(input)
+p2 = decipherCode(input, keypad, 0, 2)
 print("Part Two : " + str(p2))
