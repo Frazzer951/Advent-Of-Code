53f1bd78b2e31dcbb340a1b8d5e11116b6a1f07c
Luke Eltiste <luke343279@gmail.com>
2021-11-10 12:02:40 -0800

    2016 day 6

--- 2016/6/code.py
+++ 2016/6/code.py
@@ -2,31 +2,74 @@
 # Author = Frazzer951
 # Date = November 2021
 
+from collections import Counter
+
 with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
     input = input_file.read().split("\n")
 
 
-def part1(input):
-    pass
+def decodeMessage(input, part2=False):
+    counters = [Counter() for _ in range(len(input[0]))]
+    for row in input:
+        for i, char in enumerate(row):
+            counters[i][char] += 1
+    if not part2:
+        message = [x.most_common(1)[0][0] for x in counters]
+    else:
+        message = [x.most_common()[-1][0] for x in counters]
+    return "".join(message)
 
 
-t1 = part1([""])
+t1 = decodeMessage(
+    [
+        "eedadn",
+        "drvtee",
+        "eandsr",
+        "raavrd",
+        "atevrs",
+        "tsrnev",
+        "sdttsa",
+        "rasrtv",
+        "nssdts",
+        "ntnada",
+        "svetve",
+        "tesnvt",
+        "vntsnd",
+        "vrdear",
+        "dvrsen",
+        "enarar",
+    ]
+)
 print("Part One Test 1: " + str(t1))
-t2 = part1([""])
-print("Part One Test 2: " + str(t2))
+assert t1 == "easter"
 
-p1 = part1(input)
+p1 = decodeMessage(input)
 print("Part One : " + str(p1))
 
 
-def part2(input):
-    pass
-
-
-t3 = part2([""])
+t3 = decodeMessage(
+    [
+        "eedadn",
+        "drvtee",
+        "eandsr",
+        "raavrd",
+        "atevrs",
+        "tsrnev",
+        "sdttsa",
+        "rasrtv",
+        "nssdts",
+        "ntnada",
+        "svetve",
+        "tesnvt",
+        "vntsnd",
+        "vrdear",
+        "dvrsen",
+        "enarar",
+    ],
+    True,
+)
 print("Part Two Test 1: " + str(t3))
-t4 = part2([""])
-print("Part Two Test 2: " + str(t4))
+assert t3 == "advent"
 
-p2 = part2(input)
+p2 = decodeMessage(input, True)
 print("Part Two : " + str(p2))
