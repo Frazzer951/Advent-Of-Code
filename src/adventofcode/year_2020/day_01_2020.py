e0e5b24dd05a4a41556b526ae7a28292e0af6fcb
Luke Eltiste <luke343279@gmail.com>
2021-12-01 12:50:55 -0800

    2020 Day 1

---
+++ src/adventofcode/year_2020/day_01_2020.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+@solution_timer(2020, 1, 1)
+def part_one(input_data: List[str]):
+    input_data = [int(x) for x in input_data]
+    for num in input_data:
+        if 2020-num in input_data:
+            return num * (2020-num)
+
+    raise SolutionNotFoundException(2020, 1, 1)
+
+
+@solution_timer(2020, 1, 2)
+def part_two(input_data: List[str]):
+    input_data = [int(x) for x in input_data]
+
+    for num1 in input_data:
+        for num2 in input_data:
+            if 2020-num1-num2 in input_data:
+                return num1 * num2 * (2020-num1-num2)
+
+    raise SolutionNotFoundException(2020, 1, 2)
+
+
+if __name__ == "__main__":
+    data = get_input_for_day(2020, 1)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2020/test_day_01_2020.py
@@ -0,0 +1,16 @@
+import pytest
+from adventofcode.year_2020.day_01_2020 import part_one, part_two
+
+
+@pytest.mark.parametrize(
+    ["line", "expected"], [([1721, 979, 366, 299, 675, 1456], 514579)]
+)
+def test_part_one(line, expected):
+    assert expected == part_one(line)
+
+
+@pytest.mark.parametrize(
+    ["line", "expected"], [([1721, 979, 366, 299, 675, 1456], 241861950)]
+)
+def test_part_two(line, expected):
+    assert expected == part_two(line)
