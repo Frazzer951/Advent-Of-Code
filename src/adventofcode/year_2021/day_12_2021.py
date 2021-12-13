1985bf0800221717dd7076ebf337d5e1dbd5cd2e
Luke <luke343279@gmail.com>
2021-12-12 19:59:33 -0800

    2021 day 12

---
+++ src/adventofcode/year_2021/day_12_2021.py
@@ -0,0 +1,80 @@
+from typing import List, Dict
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def pathfinder_1(nodes: Dict[str, set[str]], node: str, history: list[str]):
+    results = []
+    new_history = history + [node]
+    if node == "end":
+        return [new_history]
+    for neighbor in nodes[node]:
+        if neighbor != "start" and (neighbor not in history or neighbor[0].isupper()):
+            new_results = pathfinder_1(nodes, neighbor, new_history)
+            results.extend(new_results)
+    return results
+
+
+def pathfinder_2(nodes: Dict[str, set[str]], node: str, history: list[str]):
+    results = []
+    new_history = history + [node]
+    if node == "end":
+        return [new_history]
+    for neighbor in nodes[node]:
+        if neighbor != "start":
+            if neighbor[0].isupper():
+                new_results = pathfinder_2(nodes, neighbor, new_history)
+                results.extend(new_results)
+            else:
+                smalls = [i for i in new_history if i[0].islower()]
+                has_two = False
+                for i in smalls:
+                    if smalls.count(i) > 1:
+                        has_two = True
+                        break
+                if (has_two and new_history.count(neighbor) < 1) or (not has_two and new_history.count(neighbor) < 2):
+                    new_results = pathfinder_2(nodes, neighbor, new_history)
+                    results.extend(new_results)
+    return results
+
+
+@solution_timer(2021, 12, 1)
+def part_one(input_data: List[str]):
+    nodes: Dict[str, set[str]] = {}
+    for line in input_data:
+        n1, n2 = line.strip().split("-")
+        if n1 in nodes:
+            nodes[n1].add(n2)
+        else:
+            nodes[n1] = {n2}
+        if n2 in nodes.keys():
+            nodes[n2].add(n1)
+        else:
+            nodes[n2] = {n1}
+    paths = pathfinder_1(nodes, "start", list())
+    return len(paths)
+
+
+@solution_timer(2021, 12, 2)
+def part_two(input_data: List[str]):
+    nodes: Dict[str, set[str]] = {}
+    for line in input_data:
+        n1, n2 = line.strip().split("-")
+        if n1 in nodes:
+            nodes[n1].add(n2)
+        else:
+            nodes[n1] = {n2}
+        if n2 in nodes.keys():
+            nodes[n2].add(n1)
+        else:
+            nodes[n2] = {n1}
+    paths = pathfinder_2(nodes, "start", list())
+    return len(paths)
+
+
+if __name__ == "__main__":
+    data = get_input_for_day(2021, 12)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2021/test_day_12_2021.py
@@ -0,0 +1,120 @@
+import pytest
+from adventofcode.year_2021.day_12_2021 import part_one, part_two
+
+
+@pytest.mark.parametrize(
+    ["line", "expected"],
+    [
+        (
+            [
+                "start-A",
+                "start-b",
+                "A-c",
+                "A-b",
+                "b-d",
+                "A-end",
+                "b-end",
+            ],
+            10,
+        ),
+        (
+            [
+                "dc-end",
+                "HN-start",
+                "start-kj",
+                "dc-start",
+                "dc-HN",
+                "LN-dc",
+                "HN-end",
+                "kj-sa",
+                "kj-HN",
+                "kj-dc",
+            ],
+            19,
+        ),
+        (
+            [
+                "fs-end",
+                "he-DX",
+                "fs-he",
+                "start-DX",
+                "pj-DX",
+                "end-zg",
+                "zg-sl",
+                "zg-pj",
+                "pj-he",
+                "RW-he",
+                "fs-DX",
+                "pj-RW",
+                "zg-RW",
+                "start-pj",
+                "he-WI",
+                "zg-he",
+                "pj-fs",
+                "start-RW",
+            ],
+            226,
+        ),
+    ],
+)
+def test_part_one(line, expected):
+    assert expected == part_one(line)
+
+
+@pytest.mark.parametrize(
+    ["line", "expected"],
+    [
+        (
+            [
+                "start-A",
+                "start-b",
+                "A-c",
+                "A-b",
+                "b-d",
+                "A-end",
+                "b-end",
+            ],
+            36,
+        ),
+        (
+            [
+                "dc-end",
+                "HN-start",
+                "start-kj",
+                "dc-start",
+                "dc-HN",
+                "LN-dc",
+                "HN-end",
+                "kj-sa",
+                "kj-HN",
+                "kj-dc",
+            ],
+            103,
+        ),
+        (
+            [
+                "fs-end",
+                "he-DX",
+                "fs-he",
+                "start-DX",
+                "pj-DX",
+                "end-zg",
+                "zg-sl",
+                "zg-pj",
+                "pj-he",
+                "RW-he",
+                "fs-DX",
+                "pj-RW",
+                "zg-RW",
+                "start-pj",
+                "he-WI",
+                "zg-he",
+                "pj-fs",
+                "start-RW",
+            ],
+            3509,
+        ),
+    ],
+)
+def test_part_two(line, expected):
+    assert expected == part_two(line)
