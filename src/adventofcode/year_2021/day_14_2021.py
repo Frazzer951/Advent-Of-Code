984cf41b2ee227df337ef2c359dc322eaa7dbe42
Luke Eltiste <luke343279@gmail.com>
2021-12-15 15:51:15 -0800

    2021 day 14

---
+++ src/adventofcode/year_2021/day_14_2021.py
@@ -0,0 +1,64 @@
+from typing import List, Dict
+
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+from collections import Counter
+from more_itertools import windowed
+
+
+@solution_timer(2021, 14, 1)
+def part_one(input_data: List[str]):
+    polymer = input_data[0]
+    input_data = input_data[2:]
+    insertions = {}
+    for line in input_data:
+        code, insert = line.strip().split(" -> ")
+        insertions[code] = insert
+
+    for _ in range(10):
+        i = 0
+        while i < len(polymer) - 1:
+            insert = insertions[polymer[i: i + 2]]
+            polymer = polymer[: i + 1] + insert + polymer[i + 1:]
+            i += 2
+    counts = Counter(polymer)
+    mostCom = counts.most_common()[0]
+    leastCom = counts.most_common()[-1]
+    return mostCom[1] - leastCom[1]
+
+
+def insert_elements_smart(pair_count: Dict[str, int], element_count: Dict[str, int], rules: Dict[str, str]):
+    new_pair_count = pair_count.copy()
+    for key in pair_count:
+        pair = list(key)
+        insert_element = rules[key]
+        element_count[insert_element] += pair_count[key]
+        new_pair_count["".join([pair[0], insert_element])] += pair_count[key]
+        new_pair_count["".join([insert_element, pair[1]])] += pair_count[key]
+        new_pair_count[key] -= pair_count[key]
+    return new_pair_count, element_count
+
+
+@solution_timer(2021, 14, 2)
+def part_two(input_data: List[str]):
+    polymer = input_data[0]
+    input_data = input_data[2:]
+    insertions = {}
+    for line in input_data:
+        code, insert = line.strip().split(" -> ")
+        insertions[code] = insert
+
+    element_count = Counter(polymer)
+    pair_count = Counter(dict.fromkeys(insertions.keys(), 0))
+    for pair in windowed(polymer, 2):
+        pair_count["".join(pair)] += 1
+    for i in range(40):
+        pair_count, element_count = insert_elements_smart(pair_count, element_count, insertions)
+    return max(element_count.values()) - min(element_count.values())
+
+
+if __name__ == "__main__":
+    data = get_input_for_day(2021, 14)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2021/test_day_14_2021.py
@@ -0,0 +1,51 @@
+from adventofcode.year_2021.day_14_2021 import part_one, part_two
+
+
+def test_part_one():
+    assert 1588 == part_one(
+        [
+            "NNCB",
+            "",
+            "CH -> B",
+            "HH -> N",
+            "CB -> H",
+            "NH -> C",
+            "HB -> C",
+            "HC -> B",
+            "HN -> C",
+            "NN -> C",
+            "BH -> H",
+            "NC -> B",
+            "NB -> B",
+            "BN -> B",
+            "BB -> N",
+            "BC -> B",
+            "CC -> N",
+            "CN -> C",
+        ]
+    )
+
+
+def test_part_two():
+    assert 2188189693529 == part_two(
+        [
+            "NNCB",
+            "",
+            "CH -> B",
+            "HH -> N",
+            "CB -> H",
+            "NH -> C",
+            "HB -> C",
+            "HC -> B",
+            "HN -> C",
+            "NN -> C",
+            "BH -> H",
+            "NC -> B",
+            "NB -> B",
+            "BN -> B",
+            "BB -> N",
+            "BC -> B",
+            "CC -> N",
+            "CN -> C",
+        ]
+    )
